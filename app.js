8b663cf7aa5ab7f174589ac0376d03e4568007d8
krc-09 <kankanarc2020@gmail.com>
2024-10-24 15:34:39 +0530

    Added routes folder to seprately handle middleware  and error page

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+
+const app = express()
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+
+
+
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-   
-    res.send('<form action ="/product" method = "POST"><input type ="text" name ="title"><input type ="number" name ="size"><button type ="submit">Add Product</button></form>');
-});
 
-app.use('/product',(req,res,next)=>{
-    console.log(JSON.stringify(req.body));
-    res.redirect('/')});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req,res,next)=>{
-   res.send('<h1>Hello express</h1>');
+app.use((req,res,next) =>{
 
-});
-
+    res.status(404).send('<h1>Page not found</h1>')
+})
 app.listen(5000);
 
